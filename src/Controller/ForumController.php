b05a92d9aa1cd87fb3867f3b98101730f1a780a2
Borixx33 <florian.borie@ynov.com>
2019-06-19 17:22:44 +0200

    add form

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -4,8 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class
 ForumController extends AbstractController
@@ -34,8 +40,28 @@ ForumController extends AbstractController
     /**
      * @Route("/forum/new", name="forum_create")
      */
-    public function create(){
-        return $this->render('form/create.html.twig');
+    public function create(Request $request, ObjectManager $manager){
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('forum_show', ['id' => $article->getId()]);
+        }
+        dump($article);
+        return $this->render('forum/create.html.twig',[
+            'formArticle' =>$form->createView()
+        ]);
     }
 
     /**
